3d018d7538da0ef3a77d6fcc54f9df5301224edf
abao917917 <heba1991@126.com>
2019-07-15 03:23:08 +0800

    发送手机验证码-发送成功

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -36,7 +36,7 @@ export default {
   data () {
     return {
       form: {
-        mobile: '13683109553',
+        mobile: '',
         code: ''
       },
       captchaObj: null
@@ -46,18 +46,24 @@ export default {
     onSubmit () {
       console.log('submit!')
     },
+    // 点击发送验证码按钮的事件处理程序
     handleSendCode () {
+      // ①取出文本框对象中的号码
       const { mobile } = this.form
+      // ②当product参数为bind时，可以调用极验验证接口
       if (this.captchaObj) {
         return this.captchaObj.verify()
       }
+      // ③发送请求
       axios({
         method: 'GET',
         url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
         // url: `http://toutiao.course.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
         // console.log(res.data)
+        // 返回的数据为res.data,取出res.data中的data对象
         const data = res.data.data
+        // 极验初始化
         window.initGeetest({
           // 以下配置参数来自服务端 SDK
           gt: data.gt,
@@ -65,14 +71,33 @@ export default {
           offline: !data.success,
           new_captcha: data.new_captcha,
           product: 'bind'
-        }, (captchaObj) => {
+        },
+        //
+        (captchaObj) => {
           this.captchaObj = captchaObj
           // 这里可以调用验证实例 captchaObj 的实例方法
           captchaObj.onReady(function () {
           // 验证码ready之后才能调用verify方法显示验证码
             captchaObj.verify()
           }).onSuccess(function () {
-            console.log('验证成功了')
+          // console.log(captchaObj.getValidate())
+            const {
+              // 解构赋值取出后，接口中重名取的别名
+              geetest_challenge: challenge,
+              geetest_seccode: seccode,
+              geetest_validate: validate } =
+           captchaObj.getValidate()
+           // getValidate方法 获取用户进行成功验证(onSuccess)所得到的结果，该结果用于进行服务端 SDK 进行二次验证。
+            axios({
+              method: '',
+              url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`,
+              parmas: {// axios发get请求时，专门用来传递query查询字符串参数
+                challenge,
+                seccode,
+                validate
+
+              }
+            })
           })
         })
       })
