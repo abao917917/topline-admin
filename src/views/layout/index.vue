f317c1f7aaaea7e3d52afbbae6de7c2700b0ce9b
abao917917 <heba1991@126.com>
2019-07-26 17:50:29 +0800

    layout组件-Container布局容器

---
+++ src/views/layout/index.vue
@@ -0,0 +1,46 @@
+<template>
+<el-container>
+  <el-aside width="200px">Aside</el-aside>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+<!--<div>
+  <AppHeader/>
+  <AppAside/>
+ 这里的内容是除了固定头和框之外的动态部分，home、发布文章、评论管理等
+</div> -->
+</template>
+
+<script>
+import AppAside from './components/AppAside'
+import AppHeader from './components/AppHeader'
+export default {
+  name: 'AppLayout',
+  components: {
+    // eslint-disable-next-line
+    AppAside,
+    // eslint-disable-next-line
+    AppHeader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang='less' scoped>
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  background: #d0dce7;
+}
+.el-header {
+  background: #aec0d1;
+}
+.el-main {
+  background: #e8eff4;
+}
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,8 +18,21 @@
               <el-input v-model="form.code" placeholder="验证码"></el-input>
             </el-col>
             <el-col :span="10" :offset="2">
-              <el-button @click="handleSendCode">获取验证码</el-button>
+              <!-- <el-button @click="handleSendCode">获取验证码</el-button> -->
+              <el-button
+                @click="handleSendCode "
+                :disabled="!!codeTimer||codeLoading"
+              >{{codeTimer ? `剩余${codeSecons}秒`:'获取验证码'}}</el-button>
             </el-col>
+          </el-form-item>
+          <el-form-item prop="agree">
+            <!-- `checked` 为 true 或 false -->
+            <el-checkbox v-model="form.agree" class="login-checkbox"></el-checkbox>
+            <span>
+              我已阅读并同意
+              <a href="#">用户协议</a> 和
+              <a href="#">隐私条款</a>
+            </span>
           </el-form-item>
           <el-form-item>
             <el-button
@@ -29,11 +42,6 @@
               :loading="loginLoading"
             >登录</el-button>
           </el-form-item>
-            <el-form-item prop="agree">
-          <!-- `checked` 为 true 或 false -->
-          <el-checkbox v-model="form.agree" class="login-checkbox"></el-checkbox>
-          <span>我已阅读并同意 <a href="#">用户协议</a> 和 <a href="#">隐私条款</a></span>
-            </el-form-item>
         </el-form>
       </div>
     </div>
@@ -43,6 +51,7 @@
 <script>
 import axios from 'axios'
 import '@/vendor/gt'
+const initCodeSeconds = 10
 export default {
   name: 'AppLogin',
   data () {
@@ -70,11 +79,15 @@ export default {
         ]
 
       },
-      captchaObj: null // 通过initGeetest得到极验验证码对象
+      captchaObj: null, // 通过initGeetest得到极验验证码对象
+      codeSecons: initCodeSeconds, // 倒计时的时间
+      codeTimer: null, // 倒计时定时器
+      sendMobile: '', // 保存第一次点击后，初始化验证码之后的手机号
+      codeLoading: false
     }
   },
   methods: {
-    // 请求登录接口
+    // 点击登录按钮，进行表单验证
     handleLogin () {
       // 表单组件有一个方法validate可以用于获取当前表单的验证状态
       this.$refs['ruleForm'].validate(valid => {
@@ -115,34 +128,53 @@ export default {
           this.loginLoading = false
         })
     },
-    // 点击发送验证码按钮的事件处理程序
+    // 点击获取验证码按钮的事件处理程序
     handleSendCode () {
-      // 校验手机号码是否有效，有效才会人机交互验证
+      // ①校验手机号码是否有效，有效才会人机交互验证
       this.$refs['ruleForm'].validateField('mobile', errorMessage => {
         // 手机号码验证判断，正确后才能显示人机交互
         if (errorMessage.trim().length > 0) {
           return
         }
-        this.showGeetest()
+
+        // ②判断是否有验证码插件对象
+        if (this.captchaObj) {
+        // 判断刷新後是不是更换了新号码
+        // 如果用户输入的手机号和之前初始化的验证码手机号不一致，就基于当前手机号码重新初始化；
+          // 否则，手机号就是和之前输入的一样，直接verify显示；
+          if (this.form.mobile !== this.sendMobile) {
+            // 处理掉验证码插件产生的旧的dom
+            document.body.removeChild(document.querySelector('.geetest_panel'))
+            // 重新初始化验证码插件
+            this.showGeetest()
+          } else {
+            // 和之前号码一致，直接调用verify显示；
+            this.captchaObj.verify()
+          }
+        } else {
+          // 这里是第1次初始化验证码插件
+          this.showGeetest()
+        }
       })
     },
+    // 调用极验，开启人机验证，发送短信
     showGeetest () {
+      // 初始化验证码期间，禁用按钮的点击状态
+      this.codeLoading = true
       // ①取出文本框对象中的号码
       const { mobile } = this.form
-      // ②当product参数为bind时，可以调用极验验证接口
-      if (this.captchaObj) {
-        return this.captchaObj.verify()
-      }
-      // ③发送请求
+
+      // ③发送请求,请求线上接口
       axios({
         method: 'GET',
-        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${this.form.mobile}`
         // url: `http://toutiao.course.itcast.cn/mp/v1_0/captchas/${mobile}`
       }).then(res => {
         // console.log(res.data)
         // 返回的数据为res.data,取出res.data中的data对象
         const data = res.data.data
         // 极验初始化
+        // 使用初始化函数 initGeetest 初始化后，它的第二个参数是一个回调，回调的第一个参数即是验证实例
         window.initGeetest(
           {
             // 以下配置参数来自服务端 SDK
@@ -152,16 +184,19 @@ export default {
             new_captcha: data.new_captcha,
             product: 'bind'
           },
-          //
+          // 极验验证实例
           captchaObj => {
             this.captchaObj = captchaObj
             // 这里可以调用验证实例 captchaObj 的实例方法
-            captchaObj
-              .onReady(function () {
-                // 验证码ready之后才能调用verify方法显示验证码
-                captchaObj.verify()
-              })
-              .onSuccess(function () {
+            captchaObj.onReady(() => {
+              // 验证码ready之后才能调用verify方法显示验证码
+              // 记录曾经点击过发送验证码按钮，初始化过的手机号码
+              this.sendMobile = this.form.mobile
+              captchaObj.verify()
+              // 验证码初始化好了，让‘获取验证码按钮’可点击
+              this.codeLoading = false
+            })
+              .onSuccess(() => { // function定义的普通函数this指向window,并不指向vue实例所以此处函数要改成箭头函数，this指向外层指向的this
                 // console.log(captchaObj.getValidate())
                 const {
                   // 解构赋值取出后，接口中重名取的别名
@@ -170,6 +205,7 @@ export default {
                   geetest_validate: validate
                 } = captchaObj.getValidate()
                 // getValidate方法 获取用户进行成功验证(onSuccess)所得到的结果，该结果用于进行服务端 SDK 进行二次验证。
+                // 请求发送验证码接口，发短信
                 axios({
                   method: 'GET',
                   url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`,
@@ -180,16 +216,29 @@ export default {
                     validate
                   }
                 }).then(res => {
-                  console.log(res.data)
+                  // 发送短信后开始倒计时
+                  this.codeCountDown()
                 })
               })
           }
         )
       })
+    },
+    // 倒计时
+    codeCountDown () {
+      this.codeTimer = window.setInterval(() => {
+        this.codeSecons--
+        if (this.codeSecons <= 0) {
+          this.codeCountDown = initCodeSeconds // 让倒计时时间回到初始状态
+          window.clearInterval(this.codeTimer) // 清除倒计时
+          this.codeTimer = null // 清除倒计时定时器的ID,就算清除了定时器，还有定时器ID，必须清除
+        }
+      }, 1000)
     }
   }
 }
 </script>
+
 <style lang='less' scoped>
 .login-wrap {
   height: 100%;
@@ -211,10 +260,10 @@ export default {
     .btn-login {
       width: 100%;
     }
-  .login-checkbox {
-    margin-right: 10px;
-  }
-  span {
+    .login-checkbox {
+      margin-right: 10px;
+    }
+    span {
       color: #666;
     }
   }
